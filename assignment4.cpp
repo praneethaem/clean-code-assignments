0ea22f5a7f98e62616a2509cc41cce811f272b05
praneethaem <68540668+praneethaem@users.noreply.github.com>
2020-07-21 22:20:21 +0530

    Update assignment4.cpp
    Updated changes

--- assignment4.cpp
+++ assignment4.cpp
@@ -1,40 +1,64 @@
 
+enum subtype 
+{ 
+    t_spinner, t_slider, t_hopper 
+}; 
+
 class Icon
 {
-    float speed, glow, energy;
-    int x, y;
-    int subtype; // spinner, slider or hopper
- 
-    bool clockwise; // need for spinner
-    bool expand; // need for spinner
-    bool vertical; // need for slider
- 
-    int distance; // need for slider
-    bool visible; // need for hopper
- 
-    int xcoord, ycoord; // need for hopper
- 
-    void spin() { }
- 
-    void slide() { }
- 
-    void hop() { }
-    // constructor must set subtype: client must pass value
-    public Icon(unsigned value)
-    {
-        subtype = value; // use enum for readability
-        // and then use conditional to set associated fields
-    }
-    public void move()
-    {
-        if (subtype == 1) { spin(); }
-        else if (subtype == 2)
-        {
-            slide();
-        }
-        else
-        {
-            hop();
-        }
-    }
+  float speed, glow, energy;
+  int x, y;
+  public :
+  static void move(subtype Icon_type);
+}Icon_obj;
+
+
+class spinner :public Icon
+{
+  bool clockwise; // need for spinner
+  bool expand; // need for spinner
+  public :
+  void spin(){}
+}spinner_obj;
+
+class slider :public Icon
+{
+  bool vertical; // need for slider
+  int distance; // need for slider
+  public :
+  void slider(){}
+}slider_obj;
+
+class hopper :public Icon
+{
+  bool visible; // need for hopper
+  int xcoord, ycoord; // need for hopper
+  public :
+  void hopper(){}
+}hopper_obj;
+
+void move(subtype Icon_type)
+{
+
+  if(Icon_type == 1)
+  {
+   spinner_obj.spin();
+  }
+  else if(Icon_type == 2)
+  {
+   slider_obj.slide();
+  }
+  else
+  {
+   hopper_obj.hop();
+  }
+}
+
+class Icon_select 
+{
+  Icon_select()
+  {
+   subtype type = 1;
+   Icon_obj.move(type) ;
+   }
 }
